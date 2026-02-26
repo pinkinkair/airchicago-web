b3538cad3dc47a0593f8c7397000b1531df2a681
pinkinkair <pinkinkair@proton.me>
2026-02-26 02:43:42 +0000

    deploy proper script

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 // vite.config.js
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   root: '.',
-  base: '/airchicago-web/'
+  base: '/airchicago-web/',
+  plugins: [react()],
 });
