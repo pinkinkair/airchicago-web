a5fc5e5fdfa66e7e41c0e812f16e22c00f791db7
pinkinkair <pinkinkair@proton.me>
2026-02-26 02:31:24 +0000

    define base in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: '.',
-  build: {
-    outDir: './dist',
-    rollupOptions: {
-      output: {
-        assetFileNames: "airchicago-web/[name].[ext]",
-        chunkFileNames: "[name].[ext]",
-        entryFileNames: "airchicago-web/[name].js",
-      },
-    },
-  },
+  base: '/airchicago-web/'
 });
