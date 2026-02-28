5f03b86b79173cf413c4d23277f979bca8cd48a6
pinkinkair <pinkinkair@proton.me>
2026-02-28 03:33:55 +0000

    show logo on main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import LogoSvg from './assets/air_logo.svg';
+
 function App() {
   return (
     <main className="app">
+      <img src={LogoSvg} alt="Activists in Resistance Logo" className="logo" />
       <h1>Activists in Resistance</h1>
       <h2>Under Construction</h2>
     </main>
