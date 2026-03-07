a0cc33225e6bd58ada3a393889d1c0953621b14e
Sal DeLego <saldelego@proton.me>
2026-03-07 00:25:36 -0600

    Proposed app structure and layout

---
+++ src/components/blocks/AppHeader.jsx
@@ -0,0 +1,55 @@
+import "./AppHeader.css";
+import LogoSvg from "../../assets/images/air_logo.svg";
+import { AppPages } from "../../assets/definitions/constants";
+
+export const AppHeader = (props) => {
+  return (
+    <header className="toolbar">
+      <div className="top">
+        {/* Left side items */}
+        <div className="left-side">
+          <a href="/">
+            <img
+              src={LogoSvg}
+              alt="Activists in Resistance Logo"
+              className="logo"
+            />
+          </a>
+          <h1>Activists in Resistance</h1>
+        </div>
+      </div>
+      <div className="bottom">
+        <div style={{ borderLeft: "2px solid rgba(0, 0, 0, 0.5)" }}>
+          <button
+            className="button"
+            disabled={props.currentPage === AppPages.Home}
+            onClick={() => props.setPage(AppPages.Home)}
+          >
+            <h3>Home</h3>
+          </button>
+          <button
+            className="button"
+            disabled={props.currentPage === AppPages.Missing}
+            onClick={() => props.setPage(AppPages.Missing)}
+          >
+            <h3>Mission</h3>
+          </button>
+          <button
+            className="button"
+            disabled={props.currentPage === AppPages.AboutUs}
+            onClick={() => props.setPage(AppPages.AboutUs)}
+          >
+            <h3>About us</h3>
+          </button>
+          <button
+            className="button"
+            disabled={props.currentPage === AppPages.ContactUs}
+            onClick={() => props.setPage(AppPages.ContactUs)}
+          >
+            <h3>Contact us</h3>
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
